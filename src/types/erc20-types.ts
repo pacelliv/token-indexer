f90b1bba66868a7b6eb221e566b9cbb82ace2f09
PacelliV <flores.eugenio03@gmail.com>
2023-11-17 20:34:23 -0500

    moved erc20 type to file

---
+++ src/types/erc20-types.ts
@@ -0,0 +1,28 @@
+export type Result = {
+    decimals?: number
+    logo?: string
+    symbol?: string
+    name?: string
+}
+
+export type TokenMetadata = {
+    id: number
+    jsonrpc: string
+    result: Result
+}
+
+type TokenBalance = {
+    balance: string | null
+    decimals?: number
+}
+
+export type TokenData = {
+    // `tokenBalance` is treated as `string` in `TokenData`
+    // but in `TokenBalancesResponse is treated as `string | null`
+    tokenBalance: TokenBalance
+    contractAddress: string
+    decimals?: number
+    logo?: string
+    symbol?: string
+    name?: string
+}
