3e3d382097b24219944243d29ab4275cce5eb680
PacelliV <flores.eugenio03@gmail.com>
2023-11-21 20:09:39 -0500

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
     theme: {
         extend: {
             screens: {
-                widescreen: {raw: "(min-aspect-ratio: 3/2)"},
-                tallscreen: {raw: "(max-aspect-ratio: 13/20)"},
+                "widescreen": {"raw": "(min-aspect-ratio: 3/2)"},
+                "tallscreen": {"raw": "(max-aspect-ratio: 13/20)"},
             },
             keyfames: {
                 "open-menu": {
